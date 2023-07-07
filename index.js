f2c603139d967ce2736a38eff757946dac4bcab0
Arjun Maudhoo <apm526@nyu.edu>
2023-07-07 11:47:53 -0400

    Add basic commands

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.login(token);
